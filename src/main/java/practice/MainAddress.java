b8d604f69524cc0c3b91b8ab2435ec63708438bd
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-04-22 01:09:43 +0530

    Added Entries Inbook Alphabetically

--- src/main/java/practice/MainAddress.java
+++ src/main/java/practice/MainAddress.java
@@ -7,21 +7,25 @@ import java.util.Scanner;
 public class MainAddress {
 
 
-    static Scanner scan = new Scanner(System.in);
+	  static Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args) {
-    	System.out.println("Welcome to Address Book System");
-        Map<String, AddressBookMain> map = new HashMap<>();
-        System.out.println("Enter Number of address book you want to add");
-        Integer count = scan.nextInt();
-        Integer i = 0;
-        while (i < count) {
-            System.out.println("Enter  book name");
-            String bookName = scan.next();
-            AddressBookMain addressBook = new AddressBookMain();
-            addressBook.operation();
-            map.put(bookName, addressBook);
-            i++;
-        }
-        System.out.println(map);
-    }}  
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Address Book System");
+	        Map<String, AddressBookMain> map = new HashMap<>();
+	        System.out.println("Enter Number of address book you want to add");
+	        Integer count = scan.nextInt();
+	        Integer i = 0;
+	        while (i < count) {
+	            System.out.println("Enter  book name");
+	            String bookName = scan.next();
+	            AddressBookMain addressBook = new AddressBookMain();
+	            addressBook.operation();
+	            map.put(bookName, addressBook);
+	            i++;
+	        }
+	        System.out.println(map);
+
+
+	    }
+
+	}
--- src/main/java/practice/Sort.java
+++ src/main/java/practice/Sort.java
@@ -3,14 +3,20 @@ package practice;
 import java.util.Comparator;
 
 public class Sort {
-	  static Comparator<PersonContact> compareCity = new Comparator<PersonContact>() {
+	 static Comparator<PersonContact> compareCity = new Comparator<PersonContact>() {
 	        public int compare(PersonContact one, PersonContact two) {
 	            return one.getCity().compareTo(two.getCity());
 	        }
 	    };
 	    static Comparator<PersonContact> compareState = new Comparator<PersonContact>() {
 	        public int compare(PersonContact one, PersonContact two) {
+
 	            return one.getState().compareTo(two.getState());
 	        }
 	    };
-	}
+	    static Comparator<PersonContact> compareFirstName = new Comparator<PersonContact>() {
+	        public int compare(PersonContact one, PersonContact two) {
+	            return one.getFirstName().compareTo(two.getFirstName());
+	        }
+	    };
+}
